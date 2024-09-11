d74229c688051110deef173bc477de73ada13b06
Tiana Lin <tianayjlin@gmail.com>
2024-09-11 07:39:54 -0400

    mock api calls, config files (tsc, js). makefile wip

---
+++ Makefile
@@ -0,0 +1,10 @@
+TSC = TSC
+FILE = api_call_template.ts
+
+build: 
+	npm run build 
+
+test: api_call_template.js
+	npm start
+
+
---
+++ api_call_template.ts
@@ -0,0 +1,45 @@
+//source: https://blog.logrocket.com/async-await-typescript/
+// https://levelup.gitconnected.com/understand-async-await-in-typescript-in-only-a-few-minutes-dedb5a18a2c
+
+
+// async function api_call (url: string): Promise{
+//     const data: any = await fetch(url)
+//     console.log(data)
+
+// }
+
+function fetchData(url: string): Promise<string> {
+    
+    return new Promise<string>((resolve, reject) => {
+        fetch(url)
+            .then(response => {
+                if (!response.ok) {
+                    reject(new Error('Network response was not ok (status ${response.status})'));
+                }
+                return response.json();
+            })
+            .then(data => {
+                console.log(data);
+                resolve(data.message);
+            })
+            .catch(error => {
+                reject(error);
+            });
+    });
+}
+
+fetchData("https://www.deckofcardsapi.com/api/deck/new/shuffle/?deck_count=1")
+
+//Snippet 1
+// const myAsynFunction = async (url: string): Promise<T> => {
+//     const { data } = await fetch(url)
+//     return data
+// }
+
+// //Snippet 2
+// const immediatelyResolvedPromise = (url: string) => {
+//     const resultPromise = new Promise((resolve, reject) => {
+//         resolve(fetch(url))
+//     })
+//     return  resultPromise
+// }
