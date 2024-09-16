173e9609d1daa159e35c4140aa2db1e55ed9c35d
Emily Loiselle <emilyannaloiselle@gmail.com>
2024-09-16 19:14:51 -0400

    Initial commit of branch

--- src/run.ts
+++ src/run.ts
@@ -44,6 +44,7 @@ async function main() {
     }
     // TODO: Call to concurrency function and scoring logic
 
+    
     // Sample output
     console.log(`{"URL":"https://www.npmjs.com/package/express", "NetScore":0, "NetScore_Latency": 0.133,"RampUp":0.5, "RampUp_Latency": 0.002, "Correctness":0.7, "Correctness_Latency":0.076, "BusFactor":-1, "BusFactor_Latency":-1, "ResponsiveMaintainer":0.6, "ResponsiveMaintainer_Latency": 0.009, "License":0, "License_Latency": 0.046}`)
   }
