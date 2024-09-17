e3ee9e8431fb4ea3816828d121df56babf15dba5
Emily Loiselle <emilyannaloiselle@gmail.com>
2024-09-17 08:54:52 -0400

    Update logic of metrics

--- src/commands/urlCommand.ts
+++ src/commands/urlCommand.ts
@@ -16,18 +16,16 @@ export async function urlCommand (argument:string) {
     }
     
     for (const url of urls) {
-      Logger.logInfo(`Processing URL: ${url}`);
-      const busFactor = new BusFactor(url);
-      busFactor.calculateScore();
-      const corScore = new Correctness(url);
-      corScore.calculateScore();
-      const rampUp = new RampUp(url);
-      rampUp.calculateScore();
-      const licScore = new License(url);
-      licScore.calculateScore();
-      const respMet = new ResponsiveMetric(url);
-      respMet.calculateScore();
+      // TODO: function to clone repository***
 
+      Logger.logInfo(`Processing URL: ${url}`);
+      const busFactor = new BusFactor(url);  // git clone
+      const corScore = new Correctness(url);
+      const rampUp = new RampUp(url); // git clone
+      const licScore = new License(url); // git api call
+      const respMet = new ResponsiveMetric(url); // git api call
+
+      const results = await Promise.all([busFactor.calculateScore(), corScore.calculateScore(), rampUp.calculateScore(), licScore.calculateScore(), respMet.calculateScore()]);
       const netScore = new NetScore().calculateScore(busFactor, corScore, licScore, rampUp, respMet);
 
       // TODO: Function to parse netScore and individual scores to NDJSON
